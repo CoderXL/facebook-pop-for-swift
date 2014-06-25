40aca77c15a67f0b7a7bed9d19706d4aacec6d3b
aleclarson <alec.stanford.larson@gmail.com>
2014-06-25 01:10:26 -0700

    Update POPProperty.swift

--- POPProperty.swift
+++ POPProperty.swift
@@ -1,60 +1,54 @@
 
 import QuartzCore
 
-enum _POPProperty: String {
-  case
-  LayerBackgroundColor = "backgroundColor",
-  LayerBounds = "bounds",
-  LayerCornerRadius = "cornerRadius",
-  LayerBorderWidth = "borderWidth",
-  LayerBorderColor = "borderColor",
-  LayerOpacity = "opacity",
-  LayerPosition = "position",
-  LayerPositionX = "positionX",
-  LayerPositionY = "positionY",
-  LayerRotation = "rotation",
-  LayerRotationX = "rotationX",
-  LayerRotationY = "rotationY",
-  LayerScaleX = "scaleX",
-  LayerScaleXY = "scaleXY",
-  LayerScaleY = "scaleY",
-  LayerSize = "size",
-  LayerSubscaleXY = "subscaleXY",
-  LayerSubtranslationX = "subtranslationX",
-  LayerSubtranslationXY = "subtranslationXY",
-  LayerSubtranslationY = "subtranslationY",
-  LayerSubtranslationZ = "subtranslationZ",
-  LayerTranslationX = "translationX",
-  LayerTranslationXY = "translationXY",
-  LayerTranslationY = "translationY",
-  LayerTranslationZ = "translationZ",
-  LayerZPosition = "zPosition",
-  LayerShadowColor = "shadowColor",
-  LayerShadowOffset = "shadowOffset",
-  LayerShadowOpacity = "shadowOpacity",
-  LayerShadowRadius = "shadowRadius",
+// CALayer
+let kPOPLayerBackgroundColor = "backgroundColor"
+let kPOPLayerBounds = "bounds"
+let kPOPLayerCornerRadius = "cornerRadius"
+let kPOPLayerBorderWidth = "borderWidth"
+let kPOPLayerBorderColor = "borderColor"
+let kPOPLayerOpacity = "opacity"
+let kPOPLayerPosition = "position"
+let kPOPLayerPositionX = "positionX"
+let kPOPLayerPositionY = "positionY"
+let kPOPLayerRotation = "rotation"
+let kPOPLayerRotationX = "rotationX"
+let kPOPLayerRotationY = "rotationY"
+let kPOPLayerScaleX = "scaleX"
+let kPOPLayerScaleXY = "scaleXY"
+let kPOPLayerScaleY = "scaleY"
+let kPOPLayerSize = "size"
+let kPOPLayerSubscaleXY = "subscaleXY"
+let kPOPLayerSubtranslationX = "subtranslationX"
+let kPOPLayerSubtranslationXY = "subtranslationXY"
+let kPOPLayerSubtranslationY = "subtranslationY"
+let kPOPLayerSubtranslationZ = "subtranslationZ"
+let kPOPLayerTranslationX = "translationX"
+let kPOPLayerTranslationXY = "translationXY"
+let kPOPLayerTranslationY = "translationY"
+let kPOPLayerTranslationZ = "translationZ"
+let kPOPLayerZPosition = "zPosition"
+let kPOPLayerShadowColor = "shadowColor"
+let kPOPLayerShadowOffset = "shadowOffset"
+let kPOPLayerShadowOpacity = "shadowOpacity"
+let kPOPLayerShadowRadius = "shadowRadius"
 
-  // CAShapeLayer
-  ShapeLayerStrokeStart = "shapeLayer.strokeStart",
-  ShapeLayerStrokeEnd = "shapeLayer.strokeEnd",
-  ShapeLayerStrokeColor = "shapeLayer.strokeColor",
+// CAShapeLayer
+let kPOPShapeLayerStrokeStart = "shapeLayer.strokeStart"
+let kPOPShapeLayerStrokeEnd = "shapeLayer.strokeEnd"
+let kPOPShapeLayerStrokeColor = "shapeLayer.strokeColor"
 
-  // UIScrollView
-  ScrollViewContentOffset = "scrollView.contentOffset",
-  ScrollViewContentSize = "scrollView.contentSize",
-  ScrollViewZoomScale = "scrollView.zoomScale",
+// UIScrollView
+let kPOPScrollViewContentOffset = "scrollView.contentOffset"
+let kPOPScrollViewContentSize = "scrollView.contentSize"
+let kPOPScrollViewZoomScale = "scrollView.zoomScale"
 
-  //UILabel
-  LabelTextColor = "label.textColor"
-}
+//UILabel
+let kPOPLabelTextColor = "label.textColor"
 
-enum _POPThreshold: CGFloat {
-  case
-  Color = 0.01,
-  Point = 1.0,
-  Scale = 0.005
-}
-
-//let POPThresholdOpacity = POPThresholdColor
-//let POPThresholdRotation = POPThresholdColor
-//let POPThresholdRadius = POPThresholdColor
+let kPOPThresholdColor = 0.01
+let kPOPThresholdPoint = 1.0
+let kPOPThresholdScale = 0.005
+//let kPOPThresholdOpacity = kPOPThresholdColor
+//let kPOPThresholdRotation = kPOPThresholdColor
+//let kPOPThresholdRadius = kPOPThresholdColor
